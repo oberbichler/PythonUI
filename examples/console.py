6d8c8893ae4e449b732cb4376bc591077455e82e
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-10-25 11:32:42 +0200

    Add color print example

---
+++ examples/console.py
@@ -0,0 +1,30 @@
+import python_ui as ui
+
+
+class Window(ui.ApplicationWindow):
+    def __init__(self):
+        super(Window, self).__init__(
+            title='Example',
+        )
+
+    def _started(self):
+        Fore, Back, Style = ui.ConsoleStyles
+
+        print('use colors like',
+              Fore.RED + 'red,',
+              Fore.GREEN + 'green,',
+              Fore.YELLOW + 'yellow,',
+              Fore.BLUE + 'blue,',
+              Fore.MAGENTA + 'magenta,',
+              Fore.CYAN + 'cyan',
+              Fore.WHITE + 'or just white',
+              Style.RESET_ALL)
+        print(Style.BRIGHT + 'write ' + Fore.RED + 'bold ' + Fore.GREEN + 
+              'text ' + Fore.BLUE + 'in ' + Fore.YELLOW + 'different ' +
+              Fore.MAGENTA + 'colors' + Style.RESET_ALL)
+        print(Style.BRIGHT + Fore.BLACK + Back.GREEN + ' or ' + Back.WHITE +
+              ' use ' + Back.RED + ' background ' + Back.YELLOW + ' colors ' +
+              Style.RESET_ALL)
+
+
+Window.run()
