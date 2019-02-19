20d2d34f37f84002bd263be3c7974593bdfdefc2
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-02-19 18:02:28 +0100

    Update example

--- examples/application_window.py
+++ examples/application_window.py
@@ -1,24 +1,26 @@
 import python_ui as ui
 
-
-class Window(ui.ApplicationWindow):
-    def __init__(self):
-        super(Window, self).__init__(
-            title='Example',
-        )
-
-    def _build_sidebar(self, builder):
-        builder.add_label(
-            label='Here is the sidebar',
-        )
-
-    def _draw(self, ax):
+class Plot(ui.PlotCanvas):
+    def plot(self, ax):
         ax.text(
             x=0.5,
             y=0.5,
             s='Here is the plot canvas',
             horizontalalignment='center',
             verticalalignment='center',
+        )
+
+class Window(ui.ApplicationWindow):
+    def __init__(self):
+        super(Window, self).__init__(
+            title='Example',
+            content=Plot,
+        )
+        self.content.redraw()
+
+    def _build_sidebar(self, builder):
+        builder.add_label(
+            label='Here is the sidebar',
         )
 
     def _started(self):
