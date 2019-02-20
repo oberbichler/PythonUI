867700a1521cfe21438e9dd70d4dd99aa0755853
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-02-20 11:21:46 +0100

    Increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='PythonUI',
-   version='0.1',
+   version='0.2.0',
    description='A simple wrapper for creating a graphical user interface in ' +
                'python using PyQt5',
    author='Thomas Oberbichler',
