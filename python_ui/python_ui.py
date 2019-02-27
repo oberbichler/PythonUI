c24ef8a1cd645e3d23ca8d5dbc9b5052b21affaa
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-02-27 09:25:38 +0100

    Fix spinbox value

--- python_ui/python_ui.py
+++ python_ui/python_ui.py
@@ -298,17 +298,17 @@ class WidgetBuilder(object):
 
         if dtype is int:
             spinbox_widget = QtWidgets.QSpinBox()
-            spinbox_widget.setValue(option.value)
             spinbox_widget.setMinimum(minimum or -2147483648)
             spinbox_widget.setMaximum(maximum or 2147483647)
             spinbox_widget.setSingleStep(step or 1)
+            spinbox_widget.setValue(option.value)
         elif dtype is float:
             spinbox_widget = QtWidgets.QDoubleSpinBox()
-            spinbox_widget.setValue(option.value)
             spinbox_widget.setMinimum(minimum or -Qt.qInf())
             spinbox_widget.setMaximum(maximum or Qt.qInf())
             spinbox_widget.setSingleStep(step or 0.1)
             spinbox_widget.setDecimals(decimals or 5)
+            spinbox_widget.setValue(option.value)
         else:
             raise ValueError(f'Invalid dtype "{dtype.__name__}"')
 
